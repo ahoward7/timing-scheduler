95e72019c1ebf5252aedc4fff7f15100427d49e3
Avery Howard <avery.d.howard@gmail.com>
2025-01-19 22:08:45 -0500

    Init

---
+++ app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="w-screen h-screen text-white bg-slate-900 p-4">
+    <NuxtPage />
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const currentTime = ref(new Date().toLocaleTimeString())
+
+onMounted(() => {
+  setInterval(() => {
+    currentTime.value = new Date().toLocaleTimeString()
+  }, 1000)
+})
+</script>
+
+<template>
+  <div class="flex flex-col w-full h-full border border-white rounded-md">
+    <div class="grow">
+      Body
+    </div>
+    <div class="relative h-20 flex justify-center items-center gap-4 bg-slate-800 border-white">
+      <div
+        class="w-10 h-10 flex justify-center items-center text-3xl bg-white text-slate-900 rounded-md cursor-pointer">
+        -
+      </div>
+      <div
+        class="w-10 h-10 flex justify-center items-center text-3xl bg-white text-slate-900 rounded-md cursor-pointer">
+        +
+      </div>
+      <div class="absolute right-4 flex justify-center items-center text-3xl font-bold px-4">
+        {{ currentTime }}
+      </div>
+    </div>
+  </div>
+</template>
