c8904ccd8a76de4e3c6b02b3f3d0e1f504884317
Avery Howard <avery.d.howard@gmail.com>
2025-02-02 14:57:07 -0500

    More bugsss

--- events.ts
+++ events.ts
@@ -7,13 +7,13 @@ const parseRelativeTime = (offsetMinutes: number): Date => {
 }
 
 const eventOffsets = [
-  { id: 1, duration: 15, title: 'Fill Waters' },
-  { id: 2, duration: 15, title: 'Appetizer' },
-  { id: 3, duration: 10, title: 'Clear Bowls, Fill Waters' },
-  { id: 4, duration: 15, title: 'Serve Entree, Start Live Music' },
-  { id: 5, duration: 10, title: 'Fill Waters' },
-  { id: 6, duration: 10, title: 'Clear Plates, Set Desserts & Drinks' },
-  { id: 7, duration: 50, title: 'Dessert & Drinks Ready' },
+  { id: 1, duration: 14, title: 'Fill Waters' },
+  { id: 2, duration: 14, title: 'Appetizer' },
+  { id: 3, duration: 9, title: 'Clear Bowls, Fill Waters' },
+  { id: 4, duration: 14, title: 'Serve Entree, Start Live Music' },
+  { id: 5, duration: 9, title: 'Fill Waters' },
+  { id: 6, duration: 9, title: 'Clear Plates, Set Desserts & Drinks' },
+  { id: 7, duration: 49, title: 'Dessert & Drinks Ready' },
   { id: 8, duration: 120, title: 'Clear Dessert Plates, Start Cleaning' },
 ]
 
@@ -22,6 +22,7 @@ const events: ValentinesEvent[] = eventOffsets.map(({ id, duration, title }) => 
   const start = parseRelativeTime(currentTimeOffset)
   currentTimeOffset += duration
   const end = parseRelativeTime(currentTimeOffset)
+  currentTimeOffset += 1
   
   return {
     id,
