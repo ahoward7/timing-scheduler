275c507ab525731980bf061fffbc16b4114e04f3
Avery Howard <avery.d.howard@gmail.com>
2025-02-02 15:05:15 -0500

    Update wording

--- events.ts
+++ events.ts
@@ -8,11 +8,11 @@ const parseRelativeTime = (offsetMinutes: number): Date => {
 
 const eventOffsets = [
   { id: 1, duration: 14, title: 'Fill Waters' },
-  { id: 2, duration: 14, title: 'Appetizer' },
-  { id: 3, duration: 9, title: 'Clear Bowls, Fill Waters' },
+  { id: 2, duration: 14, title: 'Serve Appetizer' },
+  { id: 3, duration: 9, title: 'Clear Appetizer, Fill Waters' },
   { id: 4, duration: 14, title: 'Serve Entree, Start Live Music' },
   { id: 5, duration: 9, title: 'Fill Waters' },
-  { id: 6, duration: 9, title: 'Clear Plates, Set Desserts & Drinks' },
+  { id: 6, duration: 9, title: 'Clear Plates, Head Servers - Desserts and Drinks' },
   { id: 7, duration: 49, title: 'Dessert & Drinks Ready' },
   { id: 8, duration: 120, title: 'Clear Dessert Plates, Start Cleaning' },
 ]
